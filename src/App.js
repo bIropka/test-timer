79316149ef7a80b9d4c29ac7fceb8bbe0f5173c8
Yegor Stetsiura <biropka@gmail.com>
2023-11-22 16:07:54 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [seconds, setSeconds] = useState(0)
+  const [isWorking, setIsWorking] = useState(false)
+  let timer;
+
+  useEffect(() => {
+    if (isWorking) {
+      timer = setTimeout(() => {
+        setSeconds(seconds + 1)
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(timer);
+    }
+  })
+
+  const start = () => {
+    setIsWorking(true);
+  }
+
+  const pause = () => {
+    clearTimeout(timer);
+    setIsWorking(false);
+  }
+
+  const reset = () => {
+    pause();
+    setSeconds(0);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{Math.floor(seconds / 60)} : {seconds % 60}</div>
+        <div>
+          <button onClick={start}>Start</button>
+          <button onClick={pause}>Pause</button>
+          <button onClick={reset}>Reset</button>
+        </div>
       </header>
     </div>
   );
